24fd8cb79c13850cde61c389a35f0910bbec87b3
Max Kadel <max@curationexperts.com>
2020-09-01 13:43:17 -0400

    Documentation and CI (#2)
    * Documentation
    
    * Update CI to update bundler and run rspec and rubocop
    
    * Add pairtree code
    
    Co-authored-by: Lakeisha Robinson <lakeisha.robinson@yale.edu>

--- spec/partridge_spec.rb
+++ spec/partridge_spec.rb
@@ -1,9 +1,22 @@
-RSpec.describe Partridge do
-  it "has a version number" do
+# frozen_string_literal: true
+
+RSpec.describe Partridge::Pairtree do
+  it 'has a version number' do
     expect(Partridge::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'takes an oid and gives a path with the correct prefix' do
+    expect(described_class.oid_to_pairtree(1_000_000)).to eq('00/10/00/00')
+    expect(described_class.oid_to_pairtree(1_000_001)).to eq('01/10/00/00')
+    expect(described_class.oid_to_pairtree(1_000_002)).to eq('02/10/00/00')
+    expect(described_class.oid_to_pairtree(1_000_003)).to eq('03/10/00/00')
+  end
+
+  it 'takes oids of varying lengths and puts them in predictable places' do
+    expect(described_class.oid_to_pairtree(1000)).to eq('00/10/00')
+    expect(described_class.oid_to_pairtree(12_345_600)).to eq('00/12/34/56/00')
+    expect(described_class.oid_to_pairtree(123_456_700)).to eq('00/12/34/56/70')
+    expect(described_class.oid_to_pairtree(1_234_567_800)).to eq('00/12/34/56/78/00')
+    expect(described_class.oid_to_pairtree(1_234_567_890)).to eq('90/12/34/56/78/90')
   end
 end
